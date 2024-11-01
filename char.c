8f29594d046d1e2d1301ab1b144faaef238fe211
UT <t236105@st.yamagata-u.ac.jp>
2024-11-01 19:01:00 +0900

    Add files via upload

---
+++ char.c
@@ -0,0 +1,14 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("char umaxmum：%g\n", UCHAR_MAX);
+    printf("char maxmum：%g\n",CHAR_MAX);
+    printf("char minmum：%g\n", CHAR_MIN);
+   
+
+    return 0;
+}
+}
