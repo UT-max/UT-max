00145e1208a7acc50ee9db6dccc81e5d9c28c80f
UT <t236105@st.yamagata-u.ac.jp>
2024-11-01 19:27:41 +0900

    Add files via upload

---
+++ long.c
@@ -0,0 +1,11 @@
+int main(void)
+{
+    printf("long long int absolute umaxmum：%lu\n", ULLONG_MAX);
+    printf("long long int maxmum：%lld\n",LLONG_MAX);
+    printf("long long int minmum：%lli\n", LLONG_MIN);
+   
+
+    return 0;
+}
+}
+}
