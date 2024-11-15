8961d9d5e2c0f365523d148255c2c459e9b6bbf2
UT <t236105@st.yamagata-u.ac.jp>
2024-11-15 19:16:37 +0900

    Add files via upload

---
+++ kakeru.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Oshi, Kake, Haru;
+    
+    
+    Oshi = 20;
+    Kake = 50;
+    Haru = 20;
+    
+    if (Oshi == 20)
+    printf("Oshi is 20 years old\n");
+    if (Kake != 50)
+    printf("Kake is not 20 years old\n");
+    if (Kake >= 20)
+    printf("Kake is over 20 years old\n");
+    if (Oshi < 20)
+    printf("Oshi is under 20 years old\n");
+    
+    
+    if (Oshi > Haru)
+    printf("Oshi is older than Haru\n");
+    
+    if (Kake > Oshi)
+    printf("Kake is older than Oshi\n");
+    
+
+    return 0;
+};
+}
